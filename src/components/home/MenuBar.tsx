e8f57976da4d786037c64995da365fdde316207e
Aio <AioDev.JS@gmail.com>
2022-02-18 21:19:23 +0900

    DELETE  - unused file

--- src/components/home/MenuBar.tsx
+++ src/components/home/MenuBar.tsx
@@ -1,10 +1,17 @@
-import { Box, HStack } from '@chakra-ui/react';
+import { Box, HStack, Text } from '@chakra-ui/react';
+import { useUser } from '@src/state/swr/useUser';
 import Link from 'next/link';
 import { FC } from 'react';
 
 const MenuLink: FC<{ name: string; url: string }> = ({ name, url }) => {
   return (
-    <Box as="li" listStyleType="none" fontSize="24px" fontWeight="bold" px="20px"  >
+    <Box
+      as="li"
+      listStyleType="none"
+      fontSize="24px"
+      fontWeight="bold"
+      px="20px"
+    >
       <Link href={url}>
         <a>{name}</a>
       </Link>
@@ -12,30 +19,41 @@ const MenuLink: FC<{ name: string; url: string }> = ({ name, url }) => {
   );
 };
 
-const linkList = [
-  { name: '이용가이드', url: '/abc' },
-  { name: '티켓 구입', url: '/abc' },
-  { name: '알림', url: '/abc' },
-];
-
 const MenuBar = (params) => {
+  const { data } = useUser();
+
+  const linkList = [
+    { name: '콘서트 검색', url: '/concerts' },
+    { name: '이용자', url: '/my' },
+    // { name: '로그인', url: '/login' },
+  ];
+
   return (
-    <Box as="header"   position="sticky" top="0" backgroundColor="white" boxShadow="rgb(240 240 240) 0px -1px 0px inset"  >
-      <HStack as="ul" width="full" alignItems="center" padding="20px" >
-          <Box
-            backgroundImage={
-              'url(https://static.line-scdn.net/line_live/17d6ec1a9df/img/viewing/logo_viewing_pc_0923.png)'
-            }
-            backgroundRepeat="no-repeat"
-            href="https://viewing.live.line.me"
-            width="272px"
-            height="60px"
-          >
-            {/* LINE LIVE-VIEWING */}
-          </Box>
+    <Box
+      as="header"
+      position="sticky"
+      top="0"
+      backgroundColor="white"
+      boxShadow="rgb(240 240 240) 0px -1px 0px inset"
+    >
+      <HStack as="ul" width="full" alignItems="center" padding="20px">
+        <Box
+          backgroundImage={
+            'url(https://static.line-scdn.net/line_live/17d6ec1a9df/img/viewing/logo_viewing_pc_0923.png)'
+          }
+          backgroundRepeat="no-repeat"
+          href="https://viewing.live.line.me"
+          width="272px"
+          height="60px"
+        ></Box>
         {linkList.map(({ name, url }) => (
           <MenuLink key={name} name={name} url={url} />
         ))}
+        {data ? (
+          <Text>{data.email}</Text>
+        ) : (
+          <MenuLink name="로그인" url="/login" />
+        )}
       </HStack>
     </Box>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
 import ConcertList from '@src/components/home/ConcertList';
 import MenuBar from '@src/components/home/MenuBar';
 import { useUser } from '@src/state/swr/useUser';
@@ -7,13 +7,13 @@ const HomePage = (params) => {
   const { data } = useUser();
   console.log(data);
   return (
-    <Container height="100vh">
+    <Box height="100vh" width="100vw">
       <MenuBar></MenuBar>
       <Text textStyle="h1">Event</Text>
       <Flex width="full" justifyContent="center">
         <ConcertList></ConcertList>
       </Flex>
-    </Container>
+    </Box>
   );
 };
 
