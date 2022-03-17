4351a52dbc449294d4cc035bcfd02d792733230b
Aio <AioDev.JS@gmail.com>
2022-03-18 00:06:56 +0900

    ADD - CameraAudio Swtich

--- src/components/common/meidaView/CameraSwitch.tsx
+++ src/components/common/meidaView/CameraSwitch.tsx
@@ -1,6 +1,8 @@
 import { Box, Select } from "@chakra-ui/react";
 import { toastLog } from "@src/helper/toastLog";
+import { myStreamState } from "@src/state/recoil/viewingState";
 import { ChangeEventHandler, useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
 
 // NOTE Navigator.getUserMedia() deprecated
 const getUserMedia = navigator.mediaDevices.getUserMedia;
@@ -17,17 +19,22 @@ async function getConnectedDevices(type: MediaDeviceKind) {
 const CameraSwitch = params => {
   const [cameras, setCameras] = useState<MediaDeviceInfo[]>([]);
   const [audios, setCAudios] = useState<MediaDeviceInfo[]>([]);
+  const [myStream, setMyStream] = useRecoilState(myStreamState);
+  console.log(myStream.getAudioTracks());
+  console.log(myStream.getVideoTracks()[0].label);
+  console.log(myStream.getTracks());
+  const [curCameraId, setCurCameraId] = useState<string>();
+  const [curAudioId, setCurAudioId] = useState<string>();
 
   useEffect(() => {
-    (async function setInitCamera() {
+    const deviceChangeHandler = async () => {
       const initCameras = await getConnectedDevices("videoinput");
+      const initAudios = await getConnectedDevices("audioinput");
       setCameras(initCameras);
-    })();
-
-    const deviceChangeHandler = async (event: Event) => {
-      const newCameraList = await getConnectedDevices("videoinput");
-      setCameras(newCameraList);
+      setCAudios(initAudios);
     };
+
+    deviceChangeHandler();
     navigator.mediaDevices.addEventListener("devicechange", deviceChangeHandler);
 
     return () => {
@@ -36,9 +43,23 @@ const CameraSwitch = params => {
   }, []);
 
   const handelCameraChange: ChangeEventHandler<HTMLSelectElement> = e => {
-    getUserMedia({ video: { deviceId: e.target.value } })
+    const deviceId = e.target.value;
+    getUserMedia({ video: { deviceId }, audio: curAudioId ? { deviceId: curAudioId } : true })
       .then(stream => {
-        console.log("aaa", stream);
+        setMyStream(stream);
+        setCurCameraId(deviceId);
+      })
+      .catch(err => {
+        toastLog("error", "get stream fail", "", err);
+      });
+  };
+
+  const handelAudioChange: ChangeEventHandler<HTMLSelectElement> = e => {
+    const deviceId = e.target.value;
+    getUserMedia({ audio: { deviceId }, video: curCameraId ? { deviceId: curCameraId } : true })
+      .then(stream => {
+        setMyStream(stream);
+        setCurAudioId(deviceId);
       })
       .catch(err => {
         toastLog("error", "get stream fail", "", err);
@@ -51,6 +72,11 @@ const CameraSwitch = params => {
       <Select id="cameraSelect" aria-label="카메라 선택" placeholder="Select option" onChange={handelCameraChange}>
         {cameras.map(camera => (
           <option value={camera.deviceId}>{camera.label}</option>
+        ))}
+      </Select>
+      <Select id="audioSelect" aria-label="오디오 선택" placeholder="Select option" onChange={handelAudioChange}>
+        {audios.map(audio => (
+          <option value={audio.deviceId}>{audio.label}</option>
         ))}
       </Select>
     </Box>
