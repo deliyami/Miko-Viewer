4e614017a74cc68d6497e4cdda7b3dfe009c7d26
Aio <AioDev.JS@gmail.com>
2022-03-22 19:37:13 +0900

    RF - Use ticketId instead of concertId

--- src/state/recoil/concertState.ts
+++ src/state/recoil/concertState.ts
@@ -8,40 +8,43 @@ const enterRoomIdState = atom<string>({
   default: undefined,
 });
 
+const curUserTicketState = atom<UserTicket>({
+  key: 'curUserTicket',
+  default: undefined,
+});
+
+const enterTicketDataState = selector({
+  key: 'enterConcert',
+  get: ({ get }) => {
+    return get(curUserTicketState).ticket;
+  },
+});
+
 const enterRoomIdAsyncState = selector({
   key: 'enterRoomIdAsync',
   get: async ({ get }) => {
-    const setId = get(enterRoomIdState);
-    if (setId) return setId;
+    // private room일 경우 이미 지정 받은 상태임.
+    const enterRoomId = get(enterRoomIdState);
+    if (enterRoomId) return enterRoomId;
+
+    // public room일 경우 서버에서 받아와서 접속
     axios.defaults.withCredentials = true;
-    const { data } = await axios.post<string>(
+    const { data: myRoomId } = await axios.post<string>(
       'http://localhost:3001/api/room/enter-random',
       {
         // concertId: get(enterConcertState),
-        concertId: 1111,
+        ticketId: get(enterTicketDataState).id,
       },
       {
         withCredentials: true,
       },
     );
 
-    if (data) {
-      return data;
+    if (myRoomId) {
+      return myRoomId;
     }
     toastLog('error', 'get random roomId fail', 'fail');
   },
 });
 
-const curUserTicketState = atom<UserTicket>({
-  key: 'curUserTicket',
-  default: undefined,
-});
-
-const enterConcertState = selector({
-  key: 'enterConcert',
-  get: ({ get }) => {
-    return get(curUserTicketState)?.concert;
-  },
-});
-
-export { enterConcertState, enterRoomIdState, curUserTicketState, enterRoomIdAsyncState };
+export { enterTicketDataState, enterRoomIdState, curUserTicketState, enterRoomIdAsyncState };
