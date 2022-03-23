cd75f08e0ca224bb3bef024b181135fd8327d352
deliyami <dkdidudtn@gmail.com>
2022-03-23 13:17:15 +0900

    Fix - many user motion test

--- src/components/viewing/avatar/TempModelMotion.tsx
+++ src/components/viewing/avatar/TempModelMotion.tsx
@@ -130,7 +130,7 @@ const TempModelMotion: FC<{ mediaStream: MediaStream }> = ({ mediaStream }) => {
         // AVATAR 적절하게 render 호출하는 메소드
         setBorn(model, myPeerId, poseRig, faceRig);
         countRef.current += 1;
-        if (peers && countRef.current % 10 === 0) {
+        if (peers && countRef.current % 5 === 0) {
           const data: ChatMotionInterface = {
             sender: user.data.name,
             motion: { pose: poseRig, face: faceRig },
