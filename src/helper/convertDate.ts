4215f173d84a6f4845c403210c57648b32aa6d5e
Aio <AioDev.JS@gmail.com>
2022-03-31 14:41:07 +0900

    UPDATE - convertDate

--- src/helper/convertDate.ts
+++ src/helper/convertDate.ts
@@ -15,10 +15,16 @@ const FORMAT = {
   YMD: 'YYYY/MM/DD',
   YMDHM: 'YYYY/MM/DD A hh:mm',
   YMDHMS: 'YYYY/MM/DD A hh:mm:ss',
+  HMS: 'A hh:mm:ss',
+  ISO8601NoZ: 'YYYY-MM-DDThh:mm:00',
 };
 
 const convertDate = (data: dayjs.ConfigType, format: keyof typeof FORMAT = 'YMD') => {
   return dayjs(data).format(FORMAT[format]);
+};
+
+export const convertDateUTC = (data: dayjs.ConfigType, format: keyof typeof FORMAT = 'YMD') => {
+  return dayjs.utc(data).format(FORMAT[format]);
 };
 
 export default convertDate;
